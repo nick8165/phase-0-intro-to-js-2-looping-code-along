025a3cafca52f3497df3363de31be74dca5144a5
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-09-27 14:40:22 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(people, event) {
+    for (let i = 0; i < people.length; i++) {
+        cards.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--
+    }
+}
+
+const people = [ 'Lisa', 'Kaitlin', 'Jan' ];
+let cards = [];
